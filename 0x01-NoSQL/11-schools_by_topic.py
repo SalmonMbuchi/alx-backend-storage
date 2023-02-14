2a87fcdab88d514afa3531c5d6a2dce8fd91e466
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-02-14 22:45:13 +0300

    major corrections

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """Where can I learn Python?"""
-import pymongo
 
 
 def schools_by_topic(mongo_collection, topic):
--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """Insert a document in a collection"""
-import pymongo
 
 
 def insert_school(mongo_collection, **kwargs):
     """insert a documnet based on kwargs"""
-    result = mongo_collection.insert_one(**kwargs)
+    result = mongo_collection.insert(**kwargs)
     return result.inserted_id
