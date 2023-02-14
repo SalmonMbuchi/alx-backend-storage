26041f26c32cabd6097cea1642c4c7e436de4201
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-02-14 22:12:05 +0300

    outdated methods

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Where can I learn Python?"""
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Returns the list of school with a topic"""
+    aList = []
+    aList.append(mongo_collection.find({"topics": topic}))
+    return aList
