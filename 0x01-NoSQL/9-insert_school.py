fedc76f554ea3c4b893df8e62abec7b2e96fbce5
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-02-16 14:26:01 +0300

    use correct method

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,5 +4,5 @@
 
 def insert_school(mongo_collection, **kwargs):
     """insert a documnet based on kwargs"""
-    result = mongo_collection.insert(kwargs)
+    result = mongo_collection.insert_one(kwargs)
     return result.inserted_id
