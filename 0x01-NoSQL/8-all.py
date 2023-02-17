0f361ae0108aefd3a5aee8cd0e4f1bcb8132b498
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-02-17 10:37:04 +0300

    no document in collection

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """List all documents in a collection"""
 
-if __name__ == "__main__":
-    def list_all(mongo_collection):
-        """return empty list if no document in the collection"""
-        myList = []
-        for obj in mongo_collection.find():
-            myList.append(obj)
-        return myList
+
+def list_all(mongo_collection):
+    """return empty list if no document in the collection"""
+    myList = []
+    for obj in mongo_collection.find():
+        if obj is None:
+            return []
+        myList.append(obj)     
+    return myList
