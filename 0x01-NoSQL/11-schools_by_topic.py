c1339d186ed84f8db197f44a426faa123d7420ae
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-02-17 11:29:54 +0300

    update all documents

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -5,5 +5,6 @@
 def schools_by_topic(mongo_collection, topic):
     """Returns the list of school with a topic"""
     aList = []
-    aList.append(mongo_collection.find({"topics": topic}))
+    for school in mongo_collection.find({"topics": topic}):
+        aList.append(school)
     return aList
